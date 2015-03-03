5ff771c3e8fedb1550fbef5b73656bc858b242ed
R_curtain@163.com <R_curtain@163.com>
2015-03-03 12:49:19 +0800

    Add CreatePythonSeqFile.pu

---
+++ CreatePythonSeqFile.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+
+# CreatePythonSeqFile.py: Version 1
+
+import sys
+from CreateSequenceFile import CreateSequenceFile
+
+def Usage():
+	print "CreatePythonSeqFile FilePrefix StartFileNum FileNum"
+	exit(1)
+
+def AddPythonTag(FilePrefix, StartFileNum, FileNum, FileEndfix):
+	for i in range(StartFileNum, StartFileNum + FileNum):
+		realfilename = FilePrefix + str(i) + "." + FileEndfix
+		filehandle = open(realfilename, "w+")
+		filehandle.write("#! /usr/bin/python\n\n")
+		filehandle.write("# ")
+		filehandle.write(realfilename)
+		filehandle.write(": Version 1\n")
+		filehandle.write("")
+		filehandle.close()
+
+def main():
+	if len(sys.argv) != 4 :
+		Usage()
+	
+	filename = sys.argv[1]
+	startfilenum = int(sys.argv[2])
+	filenum = int(sys.argv[3])
+	CreateSequenceFile(filename, startfilenum, filenum, 'py')
+	AddPythonTag(filename, startfilenum, filenum, 'py')
+
+if __name__ == '__main__':
+    main()
